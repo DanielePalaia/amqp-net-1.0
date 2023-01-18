442938eea1c19730ef234dd1a513d2de8ce00d5d
Daniele Palaia <dpalaia@vmware.com>
2023-01-18 18:17:48 +0100

    test amqp 1.0

---
+++ dotnet-amqp/stream_client/init/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Stream.Client;
+
+const string stream = "mixing";
+
+
+Console.WriteLine("Initializing..");
+var config = new StreamSystemConfig
+{
+    UserName = "guest",
+    Password = "guest",
+    VirtualHost = "/"
+};
+// Connect to the broker and create the system object
+// the entry point for the client.
+var system = await StreamSystem.Create(config);
+Console.WriteLine($"Delete stream: {stream}");
+// Delete the stream
+
+try
+{
+    await system.DeleteStream(stream);
+}
+catch (Exception e)
+{
+    // ignore
+    
+}
+
+Console.WriteLine($"Create Stream: {stream}");
+await system.CreateStream(new StreamSpec(stream));
---
+++ dotnet-amqp/stream_client/producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Amqp;
+using Amqp.Framing;
+using Amqp.Types;
+
+Console.WriteLine("Hello, World!");
+/// message ---> plugin --- convert amqp 1.0 to 091 -> 091 to amqp 1.0
+
+var address = new Address("amqp://guest:guest@localhost:5672");
+var connection = new Connection(address);
+var session = new Session(connection);
+
+Message message = new Message("Message from .NET 1.0");
+message.Properties = new Properties()
+{
+    MessageId = "1",
+    Subject = "test",
+    ContentType = "text/plain"
+};
+message.ApplicationProperties = new ApplicationProperties()
+{
+    Map = { { "key1", "value1" }, { "key2", 2 } }
+};
+var sender = new SenderLink(session, "mixing", "/amq/queue/test_amqp1.0");
+sender.Send(message);
+Console.WriteLine("Sent Hello AMQP!");
+
+sender.Close();
+session.Close();
+connection.Close();
